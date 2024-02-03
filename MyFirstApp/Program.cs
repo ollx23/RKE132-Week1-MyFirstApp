a3242877c9ea43c656a0b1b743e6ac36e89c2d12
Balja. <gohot.poolggmail.com.>
2024-02-03 18:43:47 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿//This is a comment to my code
+
+
+//Rakendus küsib kasutajalt sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//output
+//string - sõne
+string userName;                       
+
+userName = Console.ReadLine();       //input
+
+Console.WriteLine("Hello" + ", " + userName + "!");        //output
+
+//string interpolatsion
+
+Console.WriteLine($"Hello, {userName}!");           //output
+
+
